48443f6c67d105b1f97885bbe7f332938d3a177e
Roman <cace.200218@gmail.com>
2025-06-08 22:33:16 +0300

    cors 2

--- src/main/java/ru/bre/bugreportservice/controller/ReportController.java
+++ src/main/java/ru/bre/bugreportservice/controller/ReportController.java
@@ -2,6 +2,7 @@ package ru.bre.bugreportservice.controller;
 
 import jakarta.mail.MessagingException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,14 +11,19 @@ import org.springframework.web.multipart.MultipartFile;
 import ru.bre.bugreportservice.dto.ReportDto;
 import ru.bre.bugreportservice.service.ReportService;
 
+import java.util.Objects;
+
 @RestController
 public class ReportController {
 
     private final ReportService reportService;
 
+    private final String secretRef;
+
     @Autowired
-    public ReportController(ReportService reportService) {
+    public ReportController(ReportService reportService, @Value("${secret}") String secretRef) {
         this.reportService = reportService;
+        this.secretRef = secretRef;
     }
 
     @PostMapping("/report-send")
@@ -25,8 +31,12 @@ public class ReportController {
             @RequestParam("title") String title,
             @RequestParam("text") String text,
             @RequestParam("imageFile") MultipartFile imageFile,
-            @RequestParam("logFile") MultipartFile logFile
+            @RequestParam("logFile") MultipartFile logFile,
+            @RequestParam("secret") String secret
     ) {
+        if (!Objects.equals(secret, secretRef)) {
+            return ResponseEntity.badRequest().body("ERR!");
+        }
         try {
             reportService.report(new ReportDto(title, text), imageFile, logFile);
             return ResponseEntity.ok("OK!");
